b5a8731941cf2a1251f9523a1b3c4cf442d56cac
Batna143 <batnasankarrao@gmail.com>
2023-08-25 20:09:13 +0530

    add Details

---
+++ orders.js
@@ -0,0 +1,39 @@
+const orders =[
+    {
+        producName:'foloble mini drone',
+        productNumber:'56852',
+        paymentStatus:'due',
+        shipping:'pending'
+    },
+    {
+        producName:'Washingmachine',
+        productNumber:'6234',
+        paymentStatus:'pending',
+        shipping:'Declined'
+    },
+    {
+        producName:'Spekers',
+        productNumber:'3545',
+        paymentStatus:'accept',
+        shipping:'pending'
+    },
+    {
+        producName:'Led Tv',
+        productNumber:'5211',
+        paymentStatus:'due',
+        shipping:'Declined'
+    },
+    {
+        producName:'Books',
+        productNumber:'2352',
+        paymentStatus:'modify',
+        shipping:'Declined'
+    },
+    {
+        producName:'Tables',
+        productNumber:'56852',
+        paymentStatus:'due',
+        shipping:'pending'
+    }
+    
+]
